de806b4aaa0a6747119da0e1f544a47958c1144e
Nahuel Garcia <nahuelgarcia226@gmail.com>
2021-09-16 19:44:57 -0300

    Updated readme

--- fuel_predictor.py
+++ fuel_predictor.py
@@ -2,3 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+
+# Make numpy printouts easier to read.
+np.set_printoptions(precision=3, suppress=True)
